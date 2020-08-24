1623f4f78d3de372a34f25fa80c7ae954538d530
sagiri77iloveu <5231267@qq.com>
2020-08-24 10:38:07 +0800

    Add files via upload

---
+++ cpp1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<cstdio>
+#include <iostream>
+#include<set>
+using namespace std;
+
+int main()
+{
+    printf("1234456\n");
+    std::cout<<"123456"<<std::endl;
+    return 0;
+}
